83bf138996b5f85149b4193b835c1d85d62f1bd4
Kensuke Nagae <kyanny@gmail.com>
2014-04-22 01:28:09 +0900

    ランダムな RGB 値を得る部分のコードが重複していたのを getRandomRGB メソッドに抽出
    しかし Label にセットするために NSArray から値を取り出す部分がうまく動いておらず、常に rgb(0, 0, 0) と表示されてしまう。
    UIColor にセットする用に doubleValue を使ったのはうまくいったのだが。

--- RandomColor/RandomColorViewController.m
+++ RandomColor/RandomColorViewController.m
@@ -18,11 +18,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    srand48(time(0));
-    double red = drand48();
-    double green = drand48();
-    double blue = drand48();
-    self.subView.backgroundColor = [[UIColor alloc] initWithRed:red green:green blue:blue alpha:1.0];
+    NSArray *colors = [self getRandomRGB];
+    self.subView.backgroundColor = [[UIColor alloc] initWithRed:[[colors objectAtIndex:0] doubleValue] green:[[colors objectAtIndex:1] doubleValue] blue:[[colors objectAtIndex:2] doubleValue] alpha:1.0];
 }
 
 - (void)didReceiveMemoryWarning
@@ -32,12 +29,15 @@
 }
 
 - (IBAction)changeColor:(id)sender {
-    srand48(time(0));
-    double red = drand48();
-    double green = drand48();
-    double blue = drand48();
-    self.subView.backgroundColor = [[UIColor alloc] initWithRed:red green:green blue:blue alpha:1.0];
-    NSString *rgb = [NSString stringWithFormat:@"rgb(%f,%f,%f)", red, green, blue];
+    NSArray *colors = [self getRandomRGB];
+    self.subView.backgroundColor = [[UIColor alloc] initWithRed:[[colors objectAtIndex:0] doubleValue] green:[[colors objectAtIndex:1] doubleValue] blue:[[colors objectAtIndex:2] doubleValue] alpha:1.0];
+    NSString *rgb = [NSString stringWithFormat:@"rgb(%d,%d,%d)", [[colors objectAtIndex:0] intValue], [[colors objectAtIndex:1] intValue], [[colors objectAtIndex:2] intValue]];
     self.rgb.text = rgb;
+}
+
+- (NSArray *) getRandomRGB {
+    srand48(time(0));
+    NSArray *rgb = [[NSArray alloc] initWithObjects:[NSNumber numberWithDouble:drand48()], [NSNumber numberWithDouble:drand48()], [NSNumber numberWithDouble:drand48()], nil];
+    return rgb;
 }
 @end
