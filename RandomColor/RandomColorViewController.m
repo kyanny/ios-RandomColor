e08857328485374636eb2a2c75ff7a819ca44cfb
Kensuke Nagae <kyanny@gmail.com>
2014-04-23 02:04:42 +0900

    NSDictionaly を使ってみた。 Label は相変わらず動かず。

--- RandomColor/RandomColorViewController.m
+++ RandomColor/RandomColorViewController.m
@@ -18,8 +18,10 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    NSArray *colors = [self getRandomRGB];
-    self.subView.backgroundColor = [[UIColor alloc] initWithRed:[[colors objectAtIndex:0] doubleValue] green:[[colors objectAtIndex:1] doubleValue] blue:[[colors objectAtIndex:2] doubleValue] alpha:1.0];
+//    NSArray *colors = [self getRandomRGB];
+    NSDictionary *colors = [self getRandomRGB];
+//    self.subView.backgroundColor = [[UIColor alloc] initWithRed:[[colors objectAtIndex:0] doubleValue] green:[[colors objectAtIndex:1] doubleValue] blue:[[colors objectAtIndex:2] doubleValue] alpha:1.0];
+    self.subView.backgroundColor = [[UIColor alloc] initWithRed:[[colors objectForKey:@"red"] doubleValue] green:[[colors objectForKey:@"green"] doubleValue] blue:[[colors objectForKey:@"blue"] doubleValue] alpha:1.0];
 }
 
 - (void)didReceiveMemoryWarning
@@ -29,15 +31,21 @@
 }
 
 - (IBAction)changeColor:(id)sender {
-    NSArray *colors = [self getRandomRGB];
-    self.subView.backgroundColor = [[UIColor alloc] initWithRed:[[colors objectAtIndex:0] doubleValue] green:[[colors objectAtIndex:1] doubleValue] blue:[[colors objectAtIndex:2] doubleValue] alpha:1.0];
-    NSString *rgb = [NSString stringWithFormat:@"rgb(%d,%d,%d)", [[colors objectAtIndex:0] intValue], [[colors objectAtIndex:1] intValue], [[colors objectAtIndex:2] intValue]];
+//    NSArray *colors = [self getRandomRGB];
+    NSDictionary *colors = [self getRandomRGB];
+    self.subView.backgroundColor = [[UIColor alloc] initWithRed:[[colors objectForKey:@"red"] doubleValue] green:[[colors objectForKey:@"green"] doubleValue] blue:[[colors objectForKey:@"blue"] doubleValue] alpha:1.0];
+//    self.subView.backgroundColor = [[UIColor alloc] initWithRed:[[colors objectAtIndex:0] doubleValue] green:[[colors objectAtIndex:1] doubleValue] blue:[[colors objectAtIndex:2] doubleValue] alpha:1.0];
+    NSString *rgb = [NSString stringWithFormat:@"rgb(%d,%d,%d)", [[colors objectForKey:@"red"] intValue], [[colors objectForKey:@"green"] intValue], [[colors objectForKey:@"blue"] intValue]];
     self.rgb.text = rgb;
 }
 
-- (NSArray *) getRandomRGB {
+- (NSDictionary *) getRandomRGB {
     srand48(time(0));
-    NSArray *rgb = [[NSArray alloc] initWithObjects:[NSNumber numberWithDouble:drand48()], [NSNumber numberWithDouble:drand48()], [NSNumber numberWithDouble:drand48()], nil];
+    NSNumber *red   = [[NSNumber alloc] initWithDouble:drand48()];
+    NSNumber *green = [[NSNumber alloc] initWithDouble:drand48()];
+    NSNumber *blue  = [[NSNumber alloc] initWithDouble:drand48()];
+    NSDictionary *rgb = [[NSDictionary alloc] initWithObjectsAndKeys:red,@"red",green,@"green",blue,@"blue", nil];
+//    NSArray *rgb = [[NSArray alloc] initWithObjects:[NSNumber numberWithDouble:drand48()], [NSNumber numberWithDouble:drand48()], [NSNumber numberWithDouble:drand48()], nil];
     return rgb;
 }
 @end
