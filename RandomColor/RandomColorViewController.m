cc1a064c5ecaf494485cab0f0cacc4c221925965
Kensuke Nagae <kyanny@gmail.com>
2014-04-23 02:14:03 +0900

    背景色とラベルの文字をセットするメソッド一つ作りそれを呼び出す形にした。

--- RandomColor/RandomColorViewController.m
+++ RandomColor/RandomColorViewController.m
@@ -17,11 +17,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-//    NSArray *colors = [self getRandomRGB];
-    NSDictionary *colors = [self getRandomRGB];
-//    self.subView.backgroundColor = [[UIColor alloc] initWithRed:[[colors objectAtIndex:0] doubleValue] green:[[colors objectAtIndex:1] doubleValue] blue:[[colors objectAtIndex:2] doubleValue] alpha:1.0];
-    self.subView.backgroundColor = [[UIColor alloc] initWithRed:[[colors objectForKey:@"red"] doubleValue] green:[[colors objectForKey:@"green"] doubleValue] blue:[[colors objectForKey:@"blue"] doubleValue] alpha:1.0];
+    [self setRandomColor];
 }
 
 - (void)didReceiveMemoryWarning
@@ -31,21 +27,17 @@
 }
 
 - (IBAction)changeColor:(id)sender {
-//    NSArray *colors = [self getRandomRGB];
-    NSDictionary *colors = [self getRandomRGB];
-    self.subView.backgroundColor = [[UIColor alloc] initWithRed:[[colors objectForKey:@"red"] doubleValue] green:[[colors objectForKey:@"green"] doubleValue] blue:[[colors objectForKey:@"blue"] doubleValue] alpha:1.0];
-//    self.subView.backgroundColor = [[UIColor alloc] initWithRed:[[colors objectAtIndex:0] doubleValue] green:[[colors objectAtIndex:1] doubleValue] blue:[[colors objectAtIndex:2] doubleValue] alpha:1.0];
-    NSString *rgb = [NSString stringWithFormat:@"rgb(%d,%d,%d)", [[colors objectForKey:@"red"] intValue], [[colors objectForKey:@"green"] intValue], [[colors objectForKey:@"blue"] intValue]];
-    self.rgb.text = rgb;
+    [self setRandomColor];
 }
 
-- (NSDictionary *) getRandomRGB {
+- (void) setRandomColor {
     srand48(time(0));
-    NSNumber *red   = [[NSNumber alloc] initWithDouble:drand48()];
-    NSNumber *green = [[NSNumber alloc] initWithDouble:drand48()];
-    NSNumber *blue  = [[NSNumber alloc] initWithDouble:drand48()];
-    NSDictionary *rgb = [[NSDictionary alloc] initWithObjectsAndKeys:red,@"red",green,@"green",blue,@"blue", nil];
-//    NSArray *rgb = [[NSArray alloc] initWithObjects:[NSNumber numberWithDouble:drand48()], [NSNumber numberWithDouble:drand48()], [NSNumber numberWithDouble:drand48()], nil];
-    return rgb;
+
+    double red = drand48();
+    double green = drand48();
+    double blue = drand48();
+
+    self.subView.backgroundColor = [[UIColor alloc] initWithRed:red green:green blue:blue alpha:1.0];
+    self.rgb.text = [NSString stringWithFormat:@"rgb(%f,%f,%f)", red, green, blue];
 }
 @end
